878f0fdba5b83471fdf7d6794acf964ee388cae9
jbt <shacharab18@gmail.com>
2026-08-05 09:22:20 +0300

    ADDED YOUR NAME AND ADDRESS

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Hello World")
-print("your name")
+print(" Hello Enter your  name: ")
+name = 'shahar'
+print(" Hello Enter your address: ")
+address = 'The Button of the Sands'
+print(" Hello " , name ," How are you: " )
